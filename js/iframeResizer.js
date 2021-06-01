bb1cb508c58a364071e4dc2a5f44bb83bbf424ac
Ludwigher <ludwig.hermansson94@gmail.com>
2021-06-01 14:17:00 +0200

    workbuster winwodw

--- js/iframeResizer.js
+++ js/iframeResizer.js
@@ -1427,15 +1427,8 @@
     }
   }
 
-  if (window.jQuery) {
-    createJQueryPublicMethod(window.jQuery)
-  }
+  if (window.jQuery) { createJQueryPublicMethod(window.jQuery); }
 
-  if (typeof define === 'function' && define.amd) {
-    define([], factory)
-  } else if (typeof module === 'object' && typeof module.exports === 'object') {
-    // Node for browserfy
-    module.exports = factory()
-  }
-  window.iFrameResize = window.iFrameResize || factory()
+  window.workbuster = window.workbuster || {}
+  window.workbuster.iFrameResize = window.workbuster.iFrameResize || factory();
 })()
